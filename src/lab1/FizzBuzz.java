93c42f70d4e94d401ecd0c379d1d7d4a5168da39
roy001 <peijie.jin@mail.utoronto.ca>
2025-09-09 12:31:07 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -5,14 +5,18 @@ package lab1;
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
+        int i = 1;
+        doFizzBuzz(i);
+    }
 
-        for (int i = 1; i < 100; i++) {
+    public static void doFizzBuzz(int i) {
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
-
+            i++;
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
 
