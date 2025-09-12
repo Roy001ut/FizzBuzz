31d99ea17a8bb6cb8bd6307599496edf7eddf677
roy001 <peijie.jin@mail.utoronto.ca>
2025-09-11 20:31:50 -0400

    overload multiples

--- src/lab1/lab1/Multiples.java
+++ src/lab1/lab1/Multiples.java
@@ -1,16 +1,21 @@
 package lab1;
 
 public class Multiples {
-    public static int multiples(int limit, int a, int b) {
+    public static int multiples(int range, int a, int b) {
         int count = 0;
-        for (int i = 1; i < limit; i++){
+        for (int i = 1; i < range; i++){
             if (i % a == 0 || i % b == 0){
                 count++;
             }
-
         }
         return count;
     }
+
+    public static int multiples() {
+        return multiples(1000, 3, 5);
+    }
     public static void main(String[] args) {
         // Example: How many numbers below 1000 are divisible by 3 or 5
-        System.out.println(multiples(1000, 3, 5));}}
+        System.out.println(multiples(1000, 3, 5));
+    }
+}
